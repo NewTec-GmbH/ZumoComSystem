6f2d014e6c39d43d713d5472d4c78edcb37dca5e
Luis Moser <moser@mail.hs-ulm.de>
2021-07-20 19:50:52 +0200

    feature(server): Implement CryptoServices #14

---
+++ Coding/server/lib/CryptoServices/CryptoServices.cpp
@@ -0,0 +1,85 @@
+/*
+BSD 3-Clause License
+
+Copyright (c) 2021, NewTec GmbH
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/**
+ * @file CryptoServices.cpp
+ * @author Luis Moser
+ * @brief CryptoServices class
+ * @date 07/20/2021
+ *
+ * @{
+ */
+
+#include <CryptoServices.h>
+
+CryptoServices::CryptoServices() :
+    m_hasherInstance()
+{
+    /* Initialize the random generator with seed with entropy from analog noise */
+    randomSeed(analogRead(0));
+}
+
+CryptoServices::~CryptoServices()
+{
+}
+
+void CryptoServices::getRandomSalt(String& outputString)
+{
+    const uint8_t MAX_VALUE = 255;
+    const uint8_t NUMBER_OF_CHARS = 3;
+    char strBuffer[NUMBER_OF_CHARS];
+
+    for (uint8_t hexIdx = 0; hexIdx < SALT_LENGTH_BYTE; hexIdx++)
+    {
+        uint8_t randomNumber = static_cast<uint8_t>(random(MAX_VALUE + 1));
+        sprintf(strBuffer, "%02X", randomNumber);
+        outputString += strBuffer;
+    }
+}
+
+void CryptoServices::hashBlake2b(String& cleartext, String& salt, String& outputString)
+{
+    String inputString = cleartext + salt;
+    uint8_t outputHash[HASH_LENGTH_BYTE];
+    const uint8_t NUMBER_OF_CHARS = 3;
+    char strBuffer[NUMBER_OF_CHARS];
+
+    m_hasherInstance.reset(HASH_LENGTH_BYTE);
+    m_hasherInstance.update(inputString.c_str(), inputString.length());
+    m_hasherInstance.finalize(outputHash, HASH_LENGTH_BYTE);
+
+    for (uint8_t hexIdx = 0; hexIdx < HASH_LENGTH_BYTE; hexIdx++)
+    {
+        sprintf(strBuffer, "%02X", outputHash[hexIdx]);
+        outputString += strBuffer;
+    }
+}
