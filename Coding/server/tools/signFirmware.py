f247aeed10dbf3b8b0039e1a98e7e7f87e7ea0d0
greyes <gabryelrdiaz@gmail.com>
2022-05-12 17:12:59 +0200

    Documentation and Optimization. Header built with Bytes instead of Strings and lists

--- Coding/server/tools/signFirmware.py
+++ Coding/server/tools/signFirmware.py
@@ -38,6 +38,10 @@ from os.path import exists, getsize
 import subprocess
 
 def buildSignedFirmware() :
+    '''
+    Builds the .cpsfw file using the created binary file and its signature, including the respective header.
+    '''
+
     ID_SIZE_BYTE = 16
     TARGET_SIZE_BYTE = 16
     VERSION_SIZE_BYTE = 1
@@ -46,46 +50,52 @@ def buildSignedFirmware() :
     SIGNATURE_SIZE_BYTE = 256
     HEADER_SIZE_BYTE = ID_SIZE_BYTE + TARGET_SIZE_BYTE + VERSION_SIZE_BYTE + HASH_ALG_SIZE_BYTE + SIGN_ALG_SIZE_BYTE + SIGNATURE_SIZE_BYTE
 
-    ID = "CPSFW"
-    TARGET = "ZUMO"
-    VERSION = "\1"
-    HASH_ALG = "SHA256"
-    SIGN_ALG = "RSA2048PKCS#1v15"
+    ID = b'CPSFW'
+    TARGET = b'ZUMO'
+    VERSION = b'\1'
+    HASH_ALG = b'SHA256'
+    SIGN_ALG = b'RSA2048PKCS#1v15'
 
-    ID_ARR = list(ID) + (['\0'] * (ID_SIZE_BYTE - len(ID)))
-    TARGET_ARR = list(TARGET) + (['\0'] * (TARGET_SIZE_BYTE - len(TARGET)))
-    VERSION_ARR = list(VERSION)
-    HASH_ALG_ARR = list(HASH_ALG) + (['\0'] * (HASH_ALG_SIZE_BYTE - len(HASH_ALG)))
-    SIGN_ALG_ARR = list(SIGN_ALG) + (['\0'] * (SIGN_ALG_SIZE_BYTE - len(SIGN_ALG)))
+    HEADER = b''
+    HEADER += ID + (b'\0' * (ID_SIZE_BYTE - len(ID)))
+    HEADER += TARGET + (b'\0' * (TARGET_SIZE_BYTE - len(TARGET)))
+    HEADER += VERSION + (b'\0' * (VERSION_SIZE_BYTE - len(VERSION)))
+    HEADER += HASH_ALG + (b'\0' * (HASH_ALG_SIZE_BYTE - len(HASH_ALG)))
+    HEADER += SIGN_ALG + (b'\0' * (SIGN_ALG_SIZE_BYTE - len(SIGN_ALG)))
 
-    HEADER_ARR = ID_ARR + TARGET_ARR + VERSION_ARR + HASH_ALG_ARR + SIGN_ALG_ARR
-
-    if(HEADER_SIZE_BYTE != (len(HEADER_ARR) + SIGNATURE_SIZE_BYTE)) :
-        raise RuntimeError("Error at Building of Header")
-
-    outputFile = open("./outputFirmware.cpsfw", "w")
-
-    for byte in HEADER_ARR :
-        outputFile.write("%c" % byte)
-
-    outputFile.close()
-
-    outputFile = open("./outputFirmware.cpsfw", "ab")
-    signatureFile = open("./signature.sign", "rb")
-    firmwareFile = open("./firmware.bin", "rb")
-
-    if(getsize("./signature.sign") != SIGNATURE_SIZE_BYTE) :
-        raise RuntimeError ("Error in length of Signature File")
     
-    outputFile.write(signatureFile.read())
-    outputFile.write(firmwareFile.read())
+    if(HEADER_SIZE_BYTE != (len(HEADER) + getsize("./signature.sign"))) :
+        raise RuntimeError("Error on Header Length")
+    else:
+        outputFile = open("./outputFirmware.cpsfw", "wb")
+        signatureFile = open("./signature.sign", "rb")
+        firmwareFile = open("./firmware.bin", "rb")
+        
+        outputFile.write(HEADER)
+        outputFile.write(signatureFile.read())
+        outputFile.write(firmwareFile.read())
 
-    outputFile.close()
-    signatureFile.close()
-    firmwareFile.close()
+        outputFile.close()
+        signatureFile.close()
+        firmwareFile.close()
+    
+    return
 
 def sign_firmware (source, target, env) :
+    """
+    This function is called after the file "firmware.hex" is built.
+    - Checks that the private signing key is in the root directory.
+    - Converts the .hex file into a .bin file
+    - Creates a Signature for the binary file
+    - Calls "buildSignedFirmware" to create the final .cpsfw file.
+    - Cleans the temporary files.
 
+    Args:
+        source (SCons.Node.FS.File): Source file as file object
+        target (SCons.Node.Alias.Alias): Alias object
+        env (SCons.Script.SConscript.SConsEnvironment): Environment object
+    """
+    
     # Check for Private Key in top directory
     if not exists("./privateKey.pem"):
         print("Private Key is missing!")
@@ -125,6 +135,8 @@ def sign_firmware (source, target, env) :
         Exit(1)
     else:
         print("Successfully signed the firmware!")
+    
+    return
 
 # Always build .hex file. Assume it is always out-of-date
 env.AlwaysBuild("${BUILD_DIR}\\${PROGNAME}.hex")
