43dac30d2de3edec0d373bb29746ec76c54cb055
greyes <gabryelrdiaz@gmail.com>
2022-09-13 08:41:33 +0200

    Fix incorrect command name

--- Coding/server/lib/API/GetButtonStatusCommand.cpp
+++ Coding/server/lib/API/GetButtonStatusCommand.cpp
@@ -46,7 +46,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <Arduino.h>
 
 GetButtonStatusCommand::GetButtonStatusCommand() :
-    Command("getfirmwareinfo", NONE),
+    Command("getbuttonstatus", NONE),
     m_io(IO::getInstance())
 {
 }
